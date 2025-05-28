cf4e36f46fc6116eeffb52c82522b2039db61471
joaopredebon <joao.predebon@cresol.com.br>
2025-05-28 10:02:01 -0300

    att11

--- script.js
+++ script.js
@@ -2,9 +2,3 @@
 function abrirLinkedin() {
 	window.open("https://www.linkedin.com/in/jo%C3%A3o-carlos-predebon/", "_blank");
 }
-
-// teste de redirecionamento por tempo
-setTimeout(() => {
-	abrirLinkedin();
-}, 90000);
-
